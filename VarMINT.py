8e248eb051ad7364e73d37ed9c9ade0d20f13e3a
gneighbor <88906597+gneighbor@users.noreply.github.com>
2022-06-16 15:45:12 -0500

    Change velocity notation.
    Old:
    u = fluid velocity solution
    v = fluid velocity test function
    p = fluid pressure solution
    q = fluid pressure test function
    
    New:
    u = none, reserved for solid displacement in FSI
    v = fluid velocity solution
    dv = fluid velocity test function
    p = fluid pressure solution
    dp = fluid pressure test function
    
    The v=velocity notation also matches the paper better. The paper also uses w & q for test functions, but we choose dv & dp here so that w and q can be used for other variables in immersed FSI (likely flowrate and weights of some kind).

--- VarMINT.py
+++ VarMINT.py
@@ -6,9 +6,6 @@ formulation for finite element solution of the incompressible Navier--Stokes
 equations in FEniCS.  The particular VMS formulation considered is taken from
 
 https://doi.org/10.1007/s00466-008-0315-x
-
-The mesh velocity discussed in the linked reference is not currently included,
-but may be added in the future.
 """
 
 from dolfin import *
@@ -37,73 +34,73 @@ def meshMetric(mesh):
     G = dxiHat_dx.T*dxiHat_dx
     return G
 
-def stabilizationParameters(u,nu,G,C_I,C_t,Dt=None,scale=Constant(1.0)):
+def stabilizationParameters(v,nu,G,C_I,C_t,Dt=None,scale=Constant(1.0)):
     """
     Compute SUPS and LSIC/grad-div stabilization parameters (returned as a 
-    tuple, in that order).  Input parameters are the velocity ``u``, the
+    tuple, in that order).  Input parameters are the velocity ``v``, the
     kinematic viscosity ``nu``, the mesh metric ``G``, order-one constants
     ``C_I`` and ``C_t``, a time step ``Dt`` (which may be omitted for
     steady problems), and a scaling factor that defaults to unity.  
     """
     # The additional epsilon is needed for zero-velocity robustness
     # in the inviscid limit.
-    denom2 = inner(u,G*u) + C_I*nu*nu*inner(G,G) + DOLFIN_EPS
+    denom2 = inner(v,G*v) + C_I*nu*nu*inner(G,G) + DOLFIN_EPS
     if(Dt != None):
         denom2 += C_t/Dt**2
     tau_M = scale/sqrt(denom2)
     tau_C = 1.0/(tau_M*tr(G))
     return tau_M, tau_C
 
-def sigmaVisc(u,mu):
+def sigmaVisc(v,mu):
     """
-    The viscous part of the Cauchy stress, in terms of velocity ``u`` and
+    The viscous part of the Cauchy stress, in terms of velocity ``v`` and
     dynamic viscosity ``mu``.
     """
-    return 2.0*mu*sym(grad(u))
+    return 2.0*mu*sym(grad(v))
 
-def sigma(u,p,mu):
+def sigma(v,p,mu):
     """
-    The fluid Cauchy stress, in terms of velocity ``u``, pressure ``p``, 
+    The fluid Cauchy stress, in terms of velocity ``v``, pressure ``p``, 
     and dynamic viscosity ``mu``.
     """
-    return sigmaVisc(u,mu) - p*Identity(ufl.shape(u)[0])
+    return sigmaVisc(v,mu) - p*Identity(ufl.shape(v)[0])
 
-def resolvedDissipationForm(u,v,mu,dx=dx):
+def resolvedDissipationForm(v,dv,mu,dx=dx):
     """
-    The weak form of the viscous operator, in terms of velocity ``u``, 
-    test function ``v``, and dynamic viscosity ``mu``.  A volume integration
+    The weak form of the viscous operator, in terms of velocity ``v``, 
+    test function ``dv``, and dynamic viscosity ``mu``.  A volume integration
     measure ``dx`` can optionally be specified.
     """
-    return inner(sigmaVisc(u,mu),grad(v))*dx
+    return inner(sigmaVisc(v,mu),grad(dv))*dx
 
-def materialTimeDerivative(u,u_t=None,f=None):
+def materialTimeDerivative(v,v_t=None,f=None):
     """
-    The fluid material time derivative, in terms of the velocity ``u``, 
-    the partial time derivative ``u_t`` (which may be omitted for steady
+    The fluid material time derivative, in terms of the velocity ``v``, 
+    the partial time derivative ``v_t`` (which may be omitted for steady
     problems), and body force per unit mass, ``f``.
     """
-    DuDt = dot(u,nabla_grad(u))
-    if(u_t != None):
-        DuDt += u_t
+    DvDt = dot(v,nabla_grad(v))
+    if(v_t != None):
+        DvDt += v_t
     if(f != None):
-        DuDt -= f
-    return DuDt
+        DvDt -= f
+    return DvDt
 
-def strongResidual(u,p,mu,rho,u_t=None,f=None):
+def strongResidual(v,p,mu,rho,v_t=None,f=None):
     """
     The momenum and continuity residuals, as a tuple, of the strong PDE,
-    system, in terms of velocity ``u``, pressure ``p``, dynamic viscosity
+    system, in terms of velocity ``v``, pressure ``p``, dynamic viscosity
     ``mu``, mass density ``rho``, and, optionally, the partial time derivative
-    of velocity, ``u_t``, and a body force per unit mass, ``f``.  
+    of velocity, ``v_t``, and a body force per unit mass, ``f``.  
     """
-    DuDt = materialTimeDerivative(u,u_t,f)
+    DvDt = materialTimeDerivative(v,v_t,f)
     i,j = ufl.indices(2)
-    r_M = rho*DuDt - as_tensor(grad(sigma(u,p,mu))[i,j,j],(i,))
-    r_C = rho*div(u)
+    r_M = rho*DvDt - as_tensor(grad(sigma(v,p,mu))[i,j,j],(i,))
+    r_C = rho*div(v)
     return r_M, r_C
 
-def interiorResidual(u,p,v,q,rho,mu,mesh,G=None,
-                     u_t=None,Dt=None,
+def interiorResidual(v,p,dv,dp,rho,mu,mesh,G=None,
+                     v_t=None,Dt=None,
                      f=None,
                      C_I=Constant(3.0),
                      C_t=Constant(4.0),
@@ -113,10 +110,10 @@ def interiorResidual(u,p,v,q,rho,mu,mesh,G=None,
     This function returns the residual of the VMS formulation, minus 
     boundary terms, as a UFL ``Form``, with the assumption that boundary 
     terms will be added on top of it.  As arguments, it takes the ``mesh``, 
-    the velocity ``u``, the partial derivative of velocity w.r.t. time ``u_t``, 
-    the pressure ``p``, the test function (`v`,`q`), the time step ``Dt``, 
+    the velocity ``v``, the partial derivative of velocity w.r.t. time ``v_t``, 
+    the pressure ``p``, the test function (`dv`,`dp`), the time step ``Dt``, 
     the mass density ``rho``, the dynamic viscosity ``mu``, and a body
-    force per unit mass, ``f``.  For steady flows, ``u_t`` and ``Dt`` 
+    force per unit mass, ``f``.  For steady flows, ``v_t`` and ``Dt`` 
     can be left with their default values of ``None``.  The argument ``G``
     can optionally be given a non-``None`` value, to use an alternate
     mesh size tensor.  If left as ``None``, it will be set to the
@@ -131,16 +128,16 @@ def interiorResidual(u,p,v,q,rho,mu,mesh,G=None,
     """
 
     # Do a quick consistency check, to avoid a difficult-to-diagnose error:
-    if((u_t != None) and (Dt==None)):
+    if((v_t != None) and (Dt==None)):
         print("VarMINT WARNING: Missing time step in unsteady problem.")
-    if((Dt != None) and (u_t==None)):
+    if((Dt != None) and (v_t==None)):
         print("VarMINT WARNING: Passing time step to steady problem.")
     if G == None:
         G = meshMetric(mesh)
     nu = mu/rho
-    tau_M, tau_C = stabilizationParameters(u,nu,G,C_I,C_t,Dt,stabScale)
-    DuDt = materialTimeDerivative(u,u_t,f)
-    r_M, r_C = strongResidual(u,p,mu,rho,u_t,f)
+    tau_M, tau_C = stabilizationParameters(v,nu,G,C_I,C_t,Dt,stabScale)
+    DvDt = materialTimeDerivative(v,v_t,f)
+    r_M, r_C = strongResidual(v,p,mu,rho,v_t,f)
 
     # Ansatz for fine scale velocity and pressure.  (Note that this is
     # somewhat of an abuse of notation, in that these are dimensionally-
@@ -149,21 +146,21 @@ def interiorResidual(u,p,v,q,rho,mu,mesh,G=None,
     # from the literature and likely stems from the fact that the
     # formulation was originally developed in terms of kinematic viscosity,
     # without a separate density parameter.)
-    uPrime = -tau_M*r_M
+    vPrime = -tau_M*r_M
     pPrime = -tau_C*r_C
 
-    return (rho*inner(DuDt,v) + inner(sigma(u,p,mu),grad(v))
-            + inner(div(u),q)
-            - inner(dot(u,nabla_grad(v)) + grad(q)/rho, uPrime)
-            - inner(pPrime,div(v))
-            + inner(v,dot(uPrime,nabla_grad(u)))
-            - inner(grad(v),outer(uPrime,uPrime))/rho)*dx
+    return (rho*inner(DvDt,dv) + inner(sigma(v,p,mu),grad(dv))
+            + inner(div(v),dp)
+            - inner(dot(v,nabla_grad(dv)) + grad(dp)/rho, vPrime)
+            - inner(pPrime,div(dv))
+            + inner(dv,dot(vPrime,nabla_grad(v)))
+            - inner(grad(dv),outer(vPrime,vPrime))/rho)*dx
 
-def stableNeumannBC(traction,rho,u,v,n,g=None,ds=ds,gamma=Constant(1.0)):
+def stableNeumannBC(traction,rho,v,dv,n,g=None,ds=ds,gamma=Constant(1.0)):
     """
     This function returns the boundary contribution of a stable Neumann BC
-    corresponding to a boundary ``traction`` when the velocity ``u`` (with 
-    corresponding test function ``v``) is flowing out of the domain, 
+    corresponding to a boundary ``traction`` when the velocity ``v`` (with 
+    corresponding test function ``dv``) is flowing out of the domain, 
     as determined by comparison with the outward-pointing normal, ``n``.  
     The optional velocity ``g`` can be used to offset the boundary velocity,
     as when this term is used to obtain a(n inflow-
@@ -188,22 +185,22 @@ def stableNeumannBC(traction,rho,u,v,n,g=None,ds=ds,gamma=Constant(1.0)):
     for discussion in the context of Navier--Stokes.  
     """
     if(g==None):
-        u_minus_g = u
+        v_minus_g = v
     else:
-        u_minus_g = u-g
-    return -(inner(traction,v)
-             + gamma*rho*ufl.Min(inner(u,n),Constant(0.0))
-             *inner(u_minus_g,v))*ds
+        v_minus_g = v-g
+    return -(inner(traction,dv)
+             + gamma*rho*ufl.Min(inner(v,n),Constant(0.0))
+             *inner(v_minus_g,dv))*ds
 
-def weakDirichletBC(u,p,v,q,g,rho,mu,mesh,ds=ds,G=None,
+def weakDirichletBC(v,p,dv,dp,g,rho,mu,mesh,ds=ds,G=None,
                     sym=True,C_pen=Constant(1e3),
                     overPenalize=False):
     """
     This returns the variational form corresponding to a weakly-enforced 
     velocity Dirichlet BC, with data ``g``, on the boundary measure
     given by ``ds``, defaulting to the full boundary of the domain given by
-    ``mesh``.  It takes as parameters an unknown velocity, ``u``, 
-    unknown pressure ``p``, corresponding test functions ``v`` and ``q``, 
+    ``mesh``.  It takes as parameters an unknown velocity, ``v``, 
+    unknown pressure ``p``, corresponding test functions ``dv`` and ``dp``, 
     mass density ``rho``, and viscosity ``mu``.  Optionally, the 
     non-symmetric variant can be used by overriding ``sym``.  ``C_pen`` is
     a dimensionless scaling factor on the penalty term.  The penalty term
@@ -225,11 +222,11 @@ def weakDirichletBC(u,p,v,q,g,rho,mu,mesh,ds=ds,G=None,
         sgn = -1.0
     if G == None:
         G = meshMetric(mesh) # $\sim h^{-2}$
-    traction = sigma(u,p,mu)*n
-    consistencyTerm = stableNeumannBC(traction,rho,u,v,n,g=g,ds=ds)
-    # Note sign of ``q``, negative for stability, regardless of ``sym``.
-    adjointConsistency = -sgn*dot(sigma(v,-sgn*q,mu)*n,u-g)*ds
-    penalty = C_pen*mu*sqrt(dot(n,G*n))*dot((u-g),v)*ds
+    traction = sigma(v,p,mu)*n
+    consistencyTerm = stableNeumannBC(traction,rho,v,dv,n,g=g,ds=ds)
+    # Note sign of ``dp``, negative for stability, regardless of ``sym``.
+    adjointConsistency = -sgn*dot(sigma(dv,-sgn*dp,mu)*n,v-g)*ds
+    penalty = C_pen*mu*sqrt(dot(n,G*n))*dot((v-g),dv)*ds
     retval = consistencyTerm + adjointConsistency
     if(overPenalize or sym):
         retval += penalty
